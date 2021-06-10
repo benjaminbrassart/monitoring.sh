f7bf1ad6f0cacec1a61f4ca9ed395d5aa81bc9ab
Benjamin BRASSART <bbrassar@e3r9p8.42.fr>
2021-06-10 21:24:38 +0200

    Added CPU load

--- monitoring.sh
+++ monitoring.sh
@@ -7,18 +7,20 @@ cpu=`lscpu`
 cpu_cores=`echo "$cpu" | grep '^CPU(s)' | \
 	awk -F "$FS" '{ print $2 }'`
 cpu_threads=`echo "$cpu" | grep '^Thread(s) per core' | \
-	awk -F "$FS" -v cores=$cpu_cores '{ print cores * $2 }'`
+	awk -F "$FS" -v "cores=$cpu_cores" '{ print cores * $2 }'`
+cpu_usage=`top -bn1 | grep '^%Cpu' | cut -c 10- | xargs | \
+	awk '{ printf("%.1f%%", $1 + $3) }'`
 
 mem_mb=`free -m | grep 'Mem' | awk '{ printf("%d/%dMB", $3, $2) }'`
-mem_pr=`free | grep 'Mem' | awk '{ printf("%.2f", $3 / $2 * 100) }'`
+mem_pr=`free | grep 'Mem' | awk '{ printf("%.2f%%", $3 / $2 * 100) }'`
 
 cat << EOF
 #Architecture: `uname -a`
 #CPU cores: $cpu_cores
 #CPU threads: $cpu_threads
-#Memory usage: $mem_mb ($mem_pr%)
+#Memory usage: $mem_mb ($mem_pr)
 #Disk usage: 
-#CPU load: 
+#CPU load: $cpu_usage
 #Last boot: 
 #LVM use: 
 #TCP connections: 
