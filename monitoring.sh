9b6677b82c84a3e314c4632bc5f772e4f2282d55
Benjamin BRASSART <bbrassar@e3r9p8.42.fr>
2021-06-10 21:11:23 +0200

    Added CPU cores and CPU threads

--- monitoring.sh
+++ monitoring.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
 export LC_ALL=C
+export FS='.+:\\s*'
+
+cpu=`lscpu`
+cpu_cores=`echo "$cpu" | grep '^CPU(s)' | \
+	awk -F "$FS" '{ print $2 }'`
+cpu_threads=`echo "$cpu" | grep '^Thread(s) per core' | \
+	awk -F "$FS" -v cores=$cpu_cores '{ print cores * $2 }'`
 
 cat << EOF
 #Architecture: `uname -a`
-#CPU cores: 
-#CPU threads: 
+#CPU cores: $cpu_cores
+#CPU threads: $cpu_threads
 #Memory usage: 
 #Disk usage: 
 #CPU load: 
