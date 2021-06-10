7978932bad818e9e99d1ab918d23871521f9d92d
Benjamin BRASSART <bbrassar@e3r9p8.42.fr>
2021-06-10 20:47:37 +0200

    Added text structure

---
+++ monitoring.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cat << EOF
+#Architecture:		
+#CPU cores:			
+#CPU threads:		
+#Memory usage:		
+#Disk usage:		
+#CPU load:			
+#Last boot:			
+#LVM use:			
+#TCP connections:	
+#Logged users:		
+#Network:			
+#sudo:				
+EOF
