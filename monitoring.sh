08b5f94ddc2e014c14da1015fdb31d6351fdc862
Benjamin BRASSART <bbrassar@e3r9p8.42.fr>
2021-06-10 20:54:03 +0200

    Removed tabs

--- monitoring.sh
+++ monitoring.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+export LC_ALL=C
+
 cat << EOF
-#Architecture:		`uname -a`
-#CPU cores:			
-#CPU threads:		
-#Memory usage:		
-#Disk usage:		
-#CPU load:			
-#Last boot:			
-#LVM use:			
-#TCP connections:	
-#Logged users:		
-#Network:			
-#sudo:				
+#Architecture: `uname -a`
+#CPU cores: 
+#CPU threads: 
+#Memory usage: 
+#Disk usage: 
+#CPU load: 
+#Last boot: 
+#LVM use: 
+#TCP connections: 
+#Logged users: 
+#Network: 
+#sudo: 
 EOF
